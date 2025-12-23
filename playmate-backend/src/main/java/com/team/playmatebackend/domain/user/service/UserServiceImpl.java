8b39dd5bb773140235b36a45837e26c792b399c8
허준형 <gjwnsgud38@gmail.com>
2025-12-23 14:57:56 +0900

    Feat : SecurityFilterChain 추가 SecurityFilterChin 추가(작성중)

--- playmate-backend/src/main/java/com/team/playmatebackend/domain/user/service/UserServiceImpl.java
+++ playmate-backend/src/main/java/com/team/playmatebackend/domain/user/service/UserServiceImpl.java
@@ -5,9 +5,7 @@ import com.team.playmatebackend.domain.user.entity.User;
 import com.team.playmatebackend.domain.user.repository.UserRepository;
 import com.team.playmatebackend.global.Jwt.JwtProvider;
 import com.team.playmatebackend.domain.user.dto.UserCreateRequest;
-import com.team.playmatebackend.domain.user.entity.User;
 import com.team.playmatebackend.domain.user.entity.enums.UserRoleType;
-import com.team.playmatebackend.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -16,32 +14,33 @@ import org.springframework.transaction.annotation.Transactional;
 @Service //스프링이 빈으로 등록
 @RequiredArgsConstructor
 public abstract class UserServiceImpl implements UserService {
+
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtProvider jwtProvider;
 
 
-@Override
-public String login(LoginRequestDto request) {
-    User user = userRepository.findByUsername(request.getUsername())
-            .orElseThrow(() -> new IllegalArgumentException("아이디가 없습니다"));
+    @Override
+    public String login(LoginRequestDto request) {
+        User user = userRepository.findByUsername(request.getUsername())
+                .orElseThrow(() -> new IllegalArgumentException("아이디가 없습니다"));
 
-    if (!passwordEncoder.matches(
-            request.getPassword(),
-            user.getUserPassword())) {
-        throw new IllegalArgumentException("비밀번호다 틀렸습니다");
+        if (!passwordEncoder.matches(
+                request.getPassword(),
+                user.getUserPassword())) {
+            throw new IllegalArgumentException("비밀번호다 틀렸습니다");
+        }
+
+        return jwtProvider.createToken(user.getUserPassword());
     }
 
-    return jwtProvider.createToken(user.getUserPassword());
-}
 
-// 회원 존재 여부
+    // 회원 존재 여부
     @Transactional(readOnly = true)
     public boolean existUser(UserCreateRequest userCreateRequest) {
         return ((userRepository.existsByUserId(userCreateRequest.getUserId())) ||
                 userRepository.existByEmail(userCreateRequest.getEmail()));
     }
-
 
     /**
      * 회원가입 메서드
--- playmate-backend/src/main/java/com/team/playmatebackend/global/config/SecurityConfig.java
+++ playmate-backend/src/main/java/com/team/playmatebackend/global/config/SecurityConfig.java
@@ -2,15 +2,41 @@ package com.team.playmatebackend.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+
+    /**
+     * Security 전반적인 필터 관리
+     * !!!!작성중!!!
+     *
+     * @author 허준형
+     * @DateOfCreated 2025-12-23
+     * @DateOfEdit 2025-12-23
+     */
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        // CSRF 공격은 세션 기반에서 이루어짐
+        // JWT는 토큰 기반이라 의미가 크지 않아 Disable
+        http
+                .csrf(AbstractHttpConfigurer::disable);
+
+
+        return http.build();
     }
 }
