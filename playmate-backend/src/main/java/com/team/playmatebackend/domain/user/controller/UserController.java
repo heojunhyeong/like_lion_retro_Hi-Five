458ecbb99d9f674a42808ad2357418df386e1236
허준형 <gjwnsgud38@gmail.com>
2025-12-23 15:38:46 +0900

    temp: update user controller

--- playmate-backend/src/main/java/com/team/playmatebackend/domain/user/controller/UserController.java
+++ playmate-backend/src/main/java/com/team/playmatebackend/domain/user/controller/UserController.java
@@ -2,23 +2,27 @@ package com.team.playmatebackend.domain.user.controller;
 
 import com.team.playmatebackend.domain.user.dto.LoginRequestDto;
 import com.team.playmatebackend.domain.user.service.UserService;
+import com.team.playmatebackend.global.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController //rest api 컨트롤러
-@RequiredArgsConstructor //생성사 자동 생성
-@RequestMapping("/api/users")  //기본 url
+@RestController // REST API 컨트롤러
+@RequiredArgsConstructor // 생성자 자동 생성
+@RequestMapping("/api/users") // 기본 URL
 public class UserController {
     private final UserService userService;
 
-@PostMapping("/login")
-public ResponseEntity<String> login(@RequestBody LoginRequestDto request){  //json을 dto변환
+    /**
 
-    String token = userService.login(request); // 로그인 + JWT 발급
-    return ResponseEntity.ok(token);            // 토큰 반환
-}
+     로그인 API
+     로그인 성공 시 JWT 토큰 반환*/
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginRequestDto request) {
+        String token = userService.login(request); // 로그인 + JWT 발급
+        return ResponseEntity.ok(token);}
 }
