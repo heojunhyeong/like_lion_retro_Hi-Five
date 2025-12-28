c4a87c472ba07a06f3c48b46113eab6703ea3496
허준형 <gjwnsgud38@gmail.com>
2025-12-28 13:31:44 +0900

    Feat : 매칭방 관련 엔티티 및 Enum, ErrorCode 추가 Match 엔티티 생성, 방생성 시 설정할 실력에 관련된 Enum클래스 작성, 매칭방 관련 ErrorCode 추가

---
+++ playmate-backend/src/main/java/com/team/playmatebackend/domain/user/entity/Match.java
@@ -0,0 +1,86 @@
+package com.team.playmatebackend.domain.user.entity;
+
+import com.team.playmatebackend.domain.user.entity.enums.*;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+/**
+ * 매칭방 엔티티
+ * 방장의 조건 공개 의무와 참여 인원 조절 기능 포함
+ *
+ * @author 허준형
+ * @DateOfCreated 2025-12-28
+ * @DateOfEdit 2025-12-28
+ */
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EntityListeners(AuditingEntityListener.class)
+public class Match {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 방 제목
+    @Column(nullable = false)
+    private String title;
+
+    // 작성자 (방장)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "host_id")
+    private User host;
+
+    // 원하는 컨텐츠
+    @Enumerated(EnumType.STRING)
+    private PreferCategory category;
+
+    // 최대 인원 수
+    private int maxParticipants;
+    // 현재 인원 수
+    private int currentParticipants;
+
+    // 입장 방식 (즉시/승인)
+    @Enumerated(EnumType.STRING)
+    private EntryMethod entryMethod;
+
+    // 방장이 설정한 제한 조건들
+    // 성별
+    @Enumerated(EnumType.STRING)
+    private Gender genderRestriction;
+
+    // 나이대
+    @Enumerated(EnumType.STRING)
+    private AgeGroup ageRestriction;
+
+    // 실력
+    @Enumerated(EnumType.STRING)
+    private SkillLevel skillRestriction;
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    /**
+     * 인원이 추가될 때 현재 인원수를 1 증가시키는 비즈니스 로직
+     * FIXME: 한 번에 여러명이 동시에 신청을 하게되면 동시성 이슈가 발생할 수 있어서 해결 필요
+     *
+     * @author 허준형
+     * @DateOfCreated 2025-12-28
+     * @DateOfEdit 2025-12-28
+     */
+    public void addParticipant() {
+        if (this.currentParticipants >= this.maxParticipants) {
+            throw new IllegalStateException("정원이 초과되었습니다.");
+        }
+        this.currentParticipants++;
+    }
+}
