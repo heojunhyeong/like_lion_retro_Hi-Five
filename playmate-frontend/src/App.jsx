bc38e38f7ed8f1aba0d5a8a41dc6a90dd738a10d
jin <jin0704@gmail.com>
2026-01-03 11:07:03 +0900

    fix: conflict

--- playmate-frontend/src/App.jsx
+++ playmate-frontend/src/App.jsx
@@ -1,11 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-<<<<<<< Updated upstream
 
 import PageLayout from "./components/PageLayout/PageLayout.jsx";
 
-=======
->>>>>>> Stashed changes
 import MainPage from "./pages/MainPage.jsx";
 import ChooseGamePage from "./pages/ChooseGamePage.jsx";
 import GameRoomPage from "./pages/GameRoomPage.jsx";
@@ -16,16 +13,12 @@ import PasswordReset from "./pages/PasswordReset.jsx";
 import ChatRoom from "./pages/ChatRoom.jsx";
 import NotificationListener from "./components/NotificationListener.jsx";
 import ProfilePage from "./pages/ProfilePage.jsx";
-<<<<<<< Updated upstream
 import ProfileEditPage from "./pages/ProfileEditPage.jsx";
 import ProfilePasswordPage from "./pages/ProfilePasswordPage.jsx";
-=======
->>>>>>> Stashed changes
 
 function App() {
   return (
     <Router>
-<<<<<<< Updated upstream
       <NotificationListener />
 
       <Routes>
@@ -46,21 +39,6 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/password-reset" element={<PasswordReset />} />
       </Routes>
-=======
-      <div className="app-container">
-        {/* 로그인 상태일 때만 동작하도록 내부에서 처리됨 */}
-        <NotificationListener />
-
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/choosegame" element={<ChooseGamePage />} />
-          <Route path="/createroom" element={<CreateRoomPage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/profile" element={<ProfilePage />} />
-        </Routes>
-      </div>
->>>>>>> Stashed changes
     </Router>
   );
 }
